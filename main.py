a5e8a009eb05f44b84badd26bebd710f31b35a1b
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-20 12:56:02 +0430

    define draw hexagon function.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 import turtle
 pen = turtle.Pen()
 screen = turtle.Screen()
+def hexagon(side_length):
+    """a function to draw a hexagon."""
+    for side in range(6):
+        pen.fd(side_length)
+        pen.left(60)
