d6225e74574cc61d6980603a2c5c6371f2df5195
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-20 14:58:47 +0430

    set initialize setting-->speed, width, hide turtle

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import turtle
 # objects
 pen = turtle.Pen()
 screen = turtle.Screen()
+# initialize settings
+pen.speed(-1)
+pen.width(3)
+pen.ht()
 # variables
 rings_num = int(turtle.numinput("number of rings", "input an integer: "))
 hexagon_len = int(turtle.numinput("hexagon side length", "input an integer: "))
