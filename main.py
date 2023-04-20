bc1390248316588cc85704cf1f0212518dc92e69
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-04-20 13:24:46 +0430

    create input variables

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """write by: M. Amin"""
 import turtle
+# objects
 pen = turtle.Pen()
 screen = turtle.Screen()
+# variables
+rings_num = int(turtle.numinput("number of rings", "input an integer: "))
+hexagon_len = int(turtle.numinput("hexagon side length", "input an integer: "))
+# functions
 def hexagon(side_length):
     """a function to draw a hexagon."""
     for side in range(6):
